56ca19544d0e64752aea3b10fd9581d2d484b558
Ivan Plakhutin <i.plakhutin@gmail.com>
2023-02-15 18:15:11 +0200

    Add project files.

---
+++ Test_cruxlab/Program.cs
@@ -0,0 +1,10 @@
+﻿using Test_cruxlab;
+
+string path = "..\\requirements.txt";
+string[] requirements = new string[] { "a 1-5: abcdj", "z 2-4: asfalseiruqwo", "b 3-6: bhhkkbbjjjb" };
+
+await File.WriteAllLinesAsync(path, requirements);
+
+var validPasswords = await Validator.ValidateAsync(path);
+
+Console.WriteLine($"Number of valid passwords from file: {path} = {validPasswords}");
---
+++ Test_cruxlab/Validator.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace Test_cruxlab
+{
+    public static class Validator
+    {
+        public static async Task<int> ValidateAsync(string path)
+        {
+            var requirements = await GetRequirementsAsync(path);
+            var validPasswords = 0;
+
+            foreach (var line in requirements)
+            {
+                var claim = line.Split(' ');
+
+                var symbol = claim.FirstOrDefault();
+                var minCount = claim[1].Split(new char[] {'-', ':'}, StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
+                var maxCount = claim[1].Split(new char[] { '-', ':' }, StringSplitOptions.RemoveEmptyEntries).LastOrDefault();
+                var password = claim.LastOrDefault();
+
+                var containsCount = password.Where(s => s.ToString() == symbol).Count();
+
+                if(containsCount >= int.Parse(minCount) && containsCount <= int.Parse(maxCount)) 
+                    validPasswords++;
+            }
+
+            return validPasswords;
+        }
+
+        private static async Task<string[]> GetRequirementsAsync(string path)
+        {
+            var text = await File.ReadAllTextAsync(path);
+            var requirements = text.Split(new char[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+
+            return requirements;
+        }
+    }
+}
